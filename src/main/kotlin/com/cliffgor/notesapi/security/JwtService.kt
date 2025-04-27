854e1a064b03a8c8f18963aa6cd376221ea6c0bc
cliff-gor <cliff.gor.dev@gmail.com>
2025-04-28 02:38:50 +0300

    JWT Create function to validate access token and refreshtoken if it is invalid

--- src/main/kotlin/com/cliffgor/notesapi/security/JwtService.kt
+++ src/main/kotlin/com/cliffgor/notesapi/security/JwtService.kt
@@ -1,5 +1,6 @@
 package com.cliffgor.notesapi.security
 
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
@@ -39,6 +40,39 @@ class JwtService (
 
     fun generateRefreshToken(userId: String) : String {
         return generateToken(userId, "refresh", refreshTokenValidityMs)
+    }
+
+    fun validateAccessToken(token: String) : Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims["type"] as? String ?: return false
+        return tokenType == "access"
+    }
+
+    fun validateRefreshToken(token: String) : Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims["type"] as? String ?: return false
+        return tokenType == "refresh"
+    }
+
+//    Authorization: Bearer <token>
+    fun getUserIdFromToken(token: String): String {
+        val rawToken = if(token.startsWith("Bearer ")) {
+            token.removePrefix("Bearer ")
+        } else token
+        val claims = parseAllClaims(rawToken)?:  throw IllegalArgumentException("Invalid token.")
+        return claims.subject
+    }
+
+    private fun parseAllClaims(token: String): Claims? {
+        return try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        } catch (e: Exception) {
+            null
+        }
     }
 
 }
