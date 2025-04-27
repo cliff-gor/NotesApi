10d923818005c1290d461aac5abcfae4023fd5cc
cliff-gor <cliff.gor.dev@gmail.com>
2025-04-27 14:46:29 +0300

    Create user Create user model and what is required for a user Create a user Repository Create a hash function to hash the user password using bcryptEncoder

--- src/main/kotlin/com/cliffgor/notesapi/database/model/User.kt
+++ src/main/kotlin/com/cliffgor/notesapi/database/model/User.kt
@@ -2,7 +2,9 @@ package com.cliffgor.notesapi.database.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document("users")
 data class User (
     val email: String,
     val hashedPassword: String,
---
+++ src/main/kotlin/com/cliffgor/notesapi/database/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.cliffgor.notesapi.database.repository
+
+import com.cliffgor.notesapi.database.model.User
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface UserRepository: MongoRepository<User, ObjectId> {
+    fun findByEmail(email: String): User?
+}
